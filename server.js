264adfd643621d716cc579e304d8ee431105efa4
Eric Valverde <ericvalverde@Eric.local>
2015-04-10 09:25:30 +0100

    bug Fix

--- server.js
+++ server.js
@@ -84,7 +84,7 @@ app.post('/api/todos', function(req, res){
 
 //Delete a todo
 
-app.delete('/api/todos/:item_id', function(req, res){
+app.delete('/api/todos/:todo_id', function(req, res){
 
 	Todo.remove({
 		_id : req.params.todo_id
